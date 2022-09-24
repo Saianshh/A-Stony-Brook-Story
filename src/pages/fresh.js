afac19811bb299587b40ff76d537aaa08b907669
Sai <saichaddha@hotmail.com>
2022-09-24 17:55:20 -0400

    Updated Freshman

--- src/pages/fresh.js
+++ src/pages/fresh.js
@@ -4,16 +4,27 @@ import unionPic from '../img/union.png';
 import '../console.css';
 
 const Freshman = (props) => {
-    const canvas = props.canvas.current;
+  const canvasRef = useRef(null);
+  
+  useEffect(() => {
+    const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = window.innerHeight * .80;
     let union = new Image();
     union.src = unionPic;
     union.onload = () => {
       context.drawImage(union, 0, 0, canvas.width, canvas.height);
     }
-    return (
-        <canvas ref={props.canvas}/>
-    )
+  }, []);
+  // freshman will call a canvas
+
+  return (
+  <>
+    <h1> TEMP STRING </h1>
+    <canvas ref={canvasRef} {...props}/>
+  </>
+  );
 }
 
 export default Freshman;
