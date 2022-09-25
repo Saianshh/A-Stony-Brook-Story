6af979950cf2f592a4c77156da3023b7e4d871fe
mahir-41 <mahiralam41@gmail.com>
2022-09-25 01:17:26 -0400

    getting there part 2...

--- src/Server/server.js
+++ src/Server/server.js
@@ -4,7 +4,7 @@ var app = express();
 var server = http.createServer(app);
 app.use(express.static('public'));
 
-server.listen(7000, function () {
+server.listen(5001, function () {
   console.log('Server listening on port 3000');
 });
 
